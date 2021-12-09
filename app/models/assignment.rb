06a51b7630cd4adc96d35222a879e3aaf64ac60e
Sian Mancha <slmancha20@gmail.com>
2021-12-08 18:50:26 -0600

    Added has_many, has_many, through, and belongs_to

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -1,3 +1,4 @@
 class Assignment < ActiveRecord::Base
-    
+    has_many :grades
+    has_many :students, through: :grades
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,4 @@
 class Student < ActiveRecord::Base
-    
+    has_many :grades
+    has_many :assignments, through: :grades
 end
